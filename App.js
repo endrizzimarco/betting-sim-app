df3606152dff1ae4a9e43ee1b71c3d21daa847f3
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-08-08 19:06:00 +0100

    feat: background

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, ImageBackground } from "react-native";
+import axios from "axios";
+
+const callLambda = async () =>
+  console.log(
+    await axios.get(
+      `https://6csjpnpengggz357h33znqhdhq0crbnp.lambda-url.eu-west-1.on.aws/ `
+    )
+  );
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <ImageBackground
+        source={require("./assets/bg.jpg")}
+        style={styles.container}
+      >
+        <Text>Hello world!</Text>
+        <Button onPress={callLambda} title="Click Me"></Button>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  container: {
+    width: "100%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
